b614e93e9a00b60d3e6ec5fbad2906c9df2bea87
sklkarn <skarna@unionky.edu>
2023-10-22 17:46:56 -0400

    Latest build of target web.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,400 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   http:\/\/my.unionky.edu   http:\/\/my.unionky.edu   copyright  "
+},
+{
+  "id": "dedication-1",
+  "level": "1",
+  "url": "dedication-1.html",
+  "type": "Dedication",
+  "number": "",
+  "title": "Dedication",
+  "body": " To my spouse.  "
+},
+{
+  "id": "acknowledgement-1",
+  "level": "1",
+  "url": "acknowledgement-1.html",
+  "type": "Acknowledgements",
+  "number": "",
+  "title": "Acknowledgements",
+  "body": " I would like to thank my colleagues and students for the contributions they have made to improve this book through questionnaires, classroom discussion, and their invaluable feedback and support throughout the writing process. I am especially very grateful to John Gould and Daniel Covington for providing their critical suggestions to organize this book. Without their guidance, this book would not be what it is today. Their knowledge and expertise were instrumental in shaping my journey in writing this book. Finally, I want to express my gratitude to my family for their unwavering encouragement and patience during the many long hours I spent at my desk. Their love and support sustained me through the ups and downs of this project. Several others deserve credit for their generous help in this book.  "
+},
+{
+  "id": "meta_frontmatter-preface",
+  "level": "1",
+  "url": "meta_frontmatter-preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": " General physics is a calculus-based introductory physics book. This book is a concise form of my lecture notes that I have been delivering in my introductory level courses for many years. This text is not a replacement of any textbook that has been recommended by the instructor but is designed to support calculus-based physics materials at a rapid and an efficient way in my personal teaching activities. Students who want to use this text assume to have prerequisite knowledge of some basic mathematical skills such as geometry, algebra, trigonometry, and some advanced mathematical tools such as a differential and integral calculus.  Learning skills by memorization cannot seem to be working efficiently in learning physics. Physics is a logical and conceptual based learning hence it requires a completely different approach than simply trying to memorize lists of formulae or examples. This book is organized into two volumes that covers the syllabus of introductory physics at undergraduate level in various universities and colleges. Volume I contains Basics of Mechanics, Fluid Mechanics, and Thermodynamics.  Volume II contains Electricity, Magnetism, Waves, Acoustics, and Optics. These topics have been carefully chosen to provide an introduction to the basic concepts in physics and to give an opportunity to sharpen critical thinking and problem solving skills. The main objective of this book is to learn how to investigate and approach problems in a logical manner. This book strives to guide readers to learn physics in a logical manner. Many examples and practice problems throughout the text may be helpful to refine physical intuition.  To tackle the physical problems the book uses the 'GUESS' policy for a systematic gatheing of information and finding realistic and logistic solution. Here is what GUESS stands for   G: Given, identify the given information and sketch the information if possible.   U: Unknown, identify the unknown that we are looking for.   E: Equation\/s, set up the equation from given information.   S: Substitute, substitute the number\/s in the equation\/s.   S: Solve, solve the equation to obtain the answer.  Comments and suggestions for improvements to the text will be gratefully accepted -- Author   "
+},
+{
+  "id": "p-4",
+  "level": "2",
+  "url": "meta_frontmatter-preface.html#p-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Volume I Volume II "
+},
+{
+  "id": "subsec-charg_transf",
+  "level": "1",
+  "url": "subsec-charg_transf.html",
+  "type": "Subsection",
+  "number": "1.1.1",
+  "title": "Methods of Transferring Charges:",
+  "body": " Methods of Transferring Charges:   Charge can neither be created nor be destroyed but it can be transferred from one object to another by the following processes.   Charging by Friction: In this method two neutral objects are rubbed together against each other to transfer charges from one object to another. The object which gains electrons becomes negatively charged and the one which looses electrons becomes positively charged.   Charging by Conduction: In this method charged object are brought in contact to the other object to transfer charges. Uncharged object becomes charged after touching by the charged object. When oppositely charged object brought in contact with each other they lose their charges.   Charging by Induction: In this method an uncharged object is brought near a charged object (no physical contact) to induce a positive and negative charge on it. When charged object is in proximity of uncharged object it produces an electrical force on electrons to move them from one part of object to another. When an insulator is brought near a charged object electrons of insulator atoms experience electrical force which stretches atoms to displace center of gravity of positive charges and negative charges. This phenomenon of separation of charges is called polarization . Hence in induction positive charges are separated from negative charges to produce charges on an object.      Electrophorus:                   It is a device used to produce charges using electrostatic induction. It consists of an insulating base plate and a conducting disc (also known as electrophorus disc) with an insulated handle [ ]. At first insulating plate is rubbed with silk, fur, or wool which produces charges on the plate depending on the type of materials being rubbed together. In the figure above negative charges are produced on the base plate. Electrophorus disk is then brought in contact with this plate and then grounded. Grounding help free charges on disk to neutralize with the opposite charges from the earth (ground). Earth is an infinite source of charges and because electrons are lighter than the positive charges electrons can easily come from earth to the disk to neutralize free positive charges or they can leave the disc to the ground if they are free charges. After grounding, the disk contains only one kind of charges, it is then disconnected from ground before get removed from the plate. All the charges on the disk then spread around its surface which can be stored and used as needed. In this case positive charges are stored from the disk.    Electroscope:      It is an instrument used to detect charge on a body. It consists of two gold (or silver or aluminum) leaves suspended from a conducting rod that leads to the outside of an insulating glass container ( ). The rod has metallic ball attached at its top, when a charged object is brought near the conducting ball or make in contact with it causes the leaves to diverge because like charges repel each other. The nature of charge (positive or negative) on any object can also be determined with the help of electroscope, by making its leaves charged with the known charges.   "
+},
+{
+  "id": "p-22",
+  "level": "2",
+  "url": "subsec-charg_transf.html#p-22",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Charging by Friction: Charging by Conduction: Charging by Induction: "
+},
+{
+  "id": "fig3",
+  "level": "2",
+  "url": "subsec-charg_transf.html#fig3",
+  "type": "Figure",
+  "number": "1.1.2",
+  "title": "",
+  "body": "                "
+},
+{
+  "id": "electroscope",
+  "level": "2",
+  "url": "subsec-charg_transf.html#electroscope",
+  "type": "Figure",
+  "number": "1.1.3",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "subsec-coulmb_law",
+  "level": "1",
+  "url": "subsec-coulmb_law.html",
+  "type": "Subsection",
+  "number": "1.1.2",
+  "title": "Coulumb’s Law",
+  "body": " Coulumb's Law   The force of attraction or repulsion between any two point charges is directly proportional to the product of these charges and inversely proportional to the square of distance between them. The direction of force is always acting along the line joining these two point charges. If is a force acting on charge due to charge  is a force acting on charge due to charge and is a distance between the two charges ( ) then from Coulumb's law where is unit vector along line joining the two charges, Coulomb's constant of free space, ; and permittivity of free space, Permittivity is an ability of a medium to store electrical energy in an electric field. In other words, permittivity is the extent to which a medium permits electrical lines of force between the charges. It is a resistance of a medium in forming electric fields. It describes the effect of a material in an electrical field. The higher the permittivity, the more the material tends to reduce electrical field (due to polarization) that set up it.                 Coulumb's Law in One Dimension  Coulomb's law holds good only for two point charges but if more than two charges are located in space then the force on each of them can be determined by using the principle of superposition.    Coulomb's Law in Two and Three Dimensions  Use principle of superposition to obtain force on any charge if the charges are distributed at different points in space.   "
+},
+{
+  "id": "fig4",
+  "level": "2",
+  "url": "subsec-coulmb_law.html#fig4",
+  "type": "Figure",
+  "number": "1.1.4",
+  "title": "",
+  "body": "            "
+},
+{
+  "id": "subsec-superp_th",
+  "level": "1",
+  "url": "subsec-superp_th.html",
+  "type": "Subsection",
+  "number": "1.1.3",
+  "title": "Principle of Superposition",
+  "body": " Principle of Superposition  It states that every charge in space exerts a force on another in such a way that there are no other chrages present in that medium. Hence, the net force excerted at a point charge by two or more charges in space is a vector sum of the individual forces.  "
+},
+{
+  "id": "sec_first-example",
+  "level": "1",
+  "url": "sec_first-example.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Examples",
+  "body": " Examples  "
+},
+{
+  "id": "ex_first",
+  "level": "1",
+  "url": "ex_first.html",
+  "type": "Exercises",
+  "number": "1.3",
+  "title": "Exercise",
+  "body": "  Exercise  "
+},
+{
+  "id": "sec_second-intro",
+  "level": "1",
+  "url": "sec_second-intro.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Algebra of Vectors",
+  "body": " Algebra of Vectors  "
+},
+{
+  "id": "sec_second-examplesA",
+  "level": "1",
+  "url": "sec_second-examplesA.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Examples A",
+  "body": " Examples A  "
+},
+{
+  "id": "ex_second",
+  "level": "1",
+  "url": "ex_second.html",
+  "type": "Exercises",
+  "number": "2.3",
+  "title": "Exercise",
+  "body": "  Exercise    "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "ex_second.html#exercise-1",
+  "type": "Exercise",
+  "number": "2.3.1",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "sec_third-intro",
+  "level": "1",
+  "url": "sec_third-intro.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Algebra of Vectors",
+  "body": " Algebra of Vectors  "
+},
+{
+  "id": "sec_third-examplesA",
+  "level": "1",
+  "url": "sec_third-examplesA.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Examples A",
+  "body": " Examples A  "
+},
+{
+  "id": "ex_third",
+  "level": "1",
+  "url": "ex_third.html",
+  "type": "Exercises",
+  "number": "3.3",
+  "title": "Exercise",
+  "body": "  Exercise    "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "ex_third.html#exercise-2",
+  "type": "Exercise",
+  "number": "3.3.1",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "sec_fourth-intro",
+  "level": "1",
+  "url": "sec_fourth-intro.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Algebra of Vectors",
+  "body": " Algebra of Vectors  "
+},
+{
+  "id": "sec_fourth-examplesA",
+  "level": "1",
+  "url": "sec_fourth-examplesA.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Examples A",
+  "body": " Examples A  "
+},
+{
+  "id": "ex_fourth",
+  "level": "1",
+  "url": "ex_fourth.html",
+  "type": "Exercises",
+  "number": "4.3",
+  "title": "Exercise",
+  "body": "  Exercise    "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "ex_fourth.html#exercise-3",
+  "type": "Exercise",
+  "number": "4.3.1",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "sec_fifth-intro",
+  "level": "1",
+  "url": "sec_fifth-intro.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Algebra of Vectors",
+  "body": " Algebra of Vectors  "
+},
+{
+  "id": "sec_fifth-examplesA",
+  "level": "1",
+  "url": "sec_fifth-examplesA.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Examples A",
+  "body": " Examples A  "
+},
+{
+  "id": "ex_fifth",
+  "level": "1",
+  "url": "ex_fifth.html",
+  "type": "Exercises",
+  "number": "5.3",
+  "title": "Exercise",
+  "body": "  Exercise    "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "ex_fifth.html#exercise-4",
+  "type": "Exercise",
+  "number": "5.3.1",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "append_math",
+  "level": "1",
+  "url": "append_math.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Mathematical Relations",
+  "body": " Mathematical Relations     Algebric:    Logerithmic Formulae: A common logerithim is defined for a logerithm of base 10 and natural logerithm is defined for a base e. If then and if then , also   Similarly we can obtain the relations for natural log (ln).   Quadratic Formula: If then the root of quadratic equation is given by        Geometric Formulae:   Circumference of a circle of circle of radius r, ;  Area of a circle of circle of radius r,   Volume of a sphere of radius r,   Surface area of a sphere of radius r,   Volume area of a cylinder of radius r and height h,   Cylindrical surface area of a cylinder of radius r and height h,    Congruent Triangles: In two triangles if corresponding SAS (side angle side) or SSS (side side side) are equal then triangles are congruent.   Similar Triangles: If corresponding AAA (angle angle angle) of two triangles are equal then the triangles are called similar triangles. In that case the ratio of corresponding sides of these triangles are equal.       Trigonometric Identities: in right angled triangle                     If then                               Trigonometric Identities: in any with sides and and angles and      Law of sines:     Law of cosines:         Binomial Theorem:       Power Series:            Calculus:      Derivatives:             Integrals:                  Vectors: Draw a rectangle having adjacent sides as and Draw perpendicular on x-axis from point L as shown in figure below. The diagonal of rectangle represents resultant of the vectors and . Now    and  and        "
+},
+{
+  "id": "p-37",
+  "level": "2",
+  "url": "append_math.html#p-37",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Algebric: Logerithmic Formulae: Quadratic Formula: "
+},
+{
+  "id": "p-38",
+  "level": "2",
+  "url": "append_math.html#p-38",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Geometric Formulae: "
+},
+{
+  "id": "p-39",
+  "level": "2",
+  "url": "append_math.html#p-39",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Trigonometric Identities: "
+},
+{
+  "id": "p-40",
+  "level": "2",
+  "url": "append_math.html#p-40",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Trigonometric Identities: "
+},
+{
+  "id": "p-41",
+  "level": "2",
+  "url": "append_math.html#p-41",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Binomial Theorem: "
+},
+{
+  "id": "p-42",
+  "level": "2",
+  "url": "append_math.html#p-42",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Power Series: "
+},
+{
+  "id": "p-43",
+  "level": "2",
+  "url": "append_math.html#p-43",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Calculus: Derivatives: Integrals: "
+},
+{
+  "id": "p-44",
+  "level": "2",
+  "url": "append_math.html#p-44",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Vectors: "
+},
+{
+  "id": "appendix-2",
+  "level": "1",
+  "url": "appendix-2.html",
+  "type": "Appendix",
+  "number": "B",
+  "title": "List of Symbols",
+  "body": " List of Symbols    Electrical Circuit Symbols:     "
+},
+{
+  "id": "p-45",
+  "level": "2",
+  "url": "appendix-2.html#p-45",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Electrical Circuit Symbols: "
+},
+{
+  "id": "index-1",
+  "level": "1",
+  "url": "index-1.html",
+  "type": "Index",
+  "number": "",
+  "title": "Index",
+  "body": " Index   "
+},
+{
+  "id": "refs-list",
+  "level": "1",
+  "url": "refs-list.html",
+  "type": "References",
+  "number": "",
+  "title": "References",
+  "body": " University Physics, Young, H. D. and Freedman, Roger A., Pearson, 2015.  Physics for Scientists and Engineers, Katz, D. M., Cengage Learning, 2015.  Electricity and Magnetism, Saxena, M.C. and Arora V.P. and Prakash, S., Pragati Prakashan, 2000.  Physics of Rainbow, Suzuki, M. and Suzuki I.S, researchgate.net, 2010.  Optics, Brijlal, S., S Chandn, 1992.  Lecture notes of physics, Hewett, L. D., TAMUK, 2013.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
